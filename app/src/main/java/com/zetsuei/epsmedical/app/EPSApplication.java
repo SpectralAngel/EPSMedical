847f308d7ca68b4c51f0d94b989896260740adb5
Carlos Flores <cafg10@gmail.com>
2016-01-25 17:49:14 -0600

    Fix compile problems

--- app/src/main/java/com/zetsuei/epsmedical/app/EPSApplication.java
+++ app/src/main/java/com/zetsuei/epsmedical/app/EPSApplication.java
@@ -18,7 +18,7 @@ public class EPSApplication extends Application {
         if (mTracker == null) {
             GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
             // To enable debug logging use: adb shell setprop log.tag.GAv4 DEBUG
-            mTracker = analytics.newTracker(R.string.ga_trackingId);
+            mTracker = analytics.newTracker(R.string.tracking);
         }
         return mTracker;
     }
